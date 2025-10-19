38af96d34af855fcfe2cc730591bbbecbc927f3e
Kamohelo <kamohelotshabalala@proton.me>
2025-10-19 22:48:54 +0200

    Remove comments

--- task_2.sql
+++ task_2.sql
@@ -1,10 +1,8 @@
--- Create Authors table
 CREATE TABLE IF NOT EXISTS Authors (
     author_id INT PRIMARY KEY AUTO_INCREMENT,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Create Books table
 CREATE TABLE IF NOT EXISTS Books (
     book_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
@@ -14,7 +12,6 @@ CREATE TABLE IF NOT EXISTS Books (
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 
--- Create Customers table
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(215) NOT NULL,
@@ -22,7 +19,6 @@ CREATE TABLE IF NOT EXISTS Customers (
     address TEXT
 );
 
--- Create Orders table
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT NOT NULL,
@@ -30,7 +26,6 @@ CREATE TABLE IF NOT EXISTS Orders (
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
--- Create Order_Details table
 CREATE TABLE IF NOT EXISTS Order_details (
     orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT NOT NULL,
